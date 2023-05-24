d17246205c0c8d1c25b8544ec5fd032640864f34
nariakiiwatani <nariakiiwatani@annolab.com>
2023-05-24 11:58:39 +0900

    test window.prerenderReady flag

--- src/components/MyHelmet.tsx
+++ src/components/MyHelmet.tsx
@@ -1,5 +1,4 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
-import { useMemo } from 'react'
 import { Podcast, Episode } from '../types/podcast';
 
 type MyHelmetProps = {
@@ -13,7 +12,30 @@ const s = {
 	link: window.origin
 }
 export const MyHelmet = ({podcast:p, episode:e}:MyHelmetProps) => {
-	const info = useMemo(() => {
+	const query = new URLSearchParams(window.location.search)
+	const channel = (() => {
+		const channel = query.get('channel')
+		return channel && !channel.includes(',') 
+	})()
+	const item = query.get('item') !== null
+	const is_ready = item?p&&e:channel?p:true
+	if(!is_ready) {
+		return (<HelmetProvider>
+		<Helmet
+			title={s.title}
+			meta={[
+				{ name: 'twitter:card', content: 'summary_large_image' },
+				{ property: 'og:title', content: s.title },
+				{ property: 'og:type', content: 'website' },
+				{ property: 'og:url', content: s.link },
+				{ property: 'og:image', content: s.imageUrl },
+				{ property: 'og:description', content: s.description },
+			]}
+		/>
+	</HelmetProvider>)
+	}
+	(window as unknown as {prerenderReady:boolean}).prerenderReady = true
+	const info = (() => {
 		const title = (e?e.title:'')+(p?e?` (${p.title})`:p.title:'')
 		return {
 			title,
@@ -22,13 +44,12 @@ export const MyHelmet = ({podcast:p, episode:e}:MyHelmetProps) => {
 			image: e?e.imageUrl:p?p.imageUrl:s.imageUrl,
 			description: e?e.description:p?p.description:s.description,
 		}
-	}, [p, e])
+	})()
 	return (<HelmetProvider>
 		<Helmet
 			title={info.title}
 			meta={[
 				{ name: 'twitter:card', content: 'summary_large_image' },
-				{ name: 'twitter:site', content: '@nariakiiwatani' },
 				{ property: 'og:title', content: info.title },
 				{ property: 'og:type', content: 'website' },
 				{ property: 'og:url', content: info.url },
