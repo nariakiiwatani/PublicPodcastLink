86813cf972c16cbf3c6d52462b4ee309f2cbce39
nariakiiwatani <nariakiiwatani@annolab.com>
2023-06-09 18:39:11 +0900

    URLにカンマを使用できなくする

--- src/components/playlist/PlaylistEditor.tsx
+++ src/components/playlist/PlaylistEditor.tsx
@@ -78,10 +78,10 @@ export const PlaylistChannelEditor = React.forwardRef<PlaylistChannelEditorRef, 
 			setAliasError('入力してください')
 			return
 		}
-		const invalid_chars = v.split('').filter(c => !/^[a-zA-Z0-9-._~!$&'()*+,;=:@]$/.test(c))
+		const invalid_chars = v.split('').filter(c => !/^[a-zA-Z0-9-._~!$&'()*+;=:@]$/.test(c))
 		if(invalid_chars.length > 0) {
 			const unique = [...new Set(invalid_chars)];
-			setAliasError(`URLに使用できない文字が含まれています: ${unique.join('')}`)
+			setAliasError(`使用できない文字が含まれています: ${unique.join('')}`)
 			return
 		}
 		setAliasPending(true)
