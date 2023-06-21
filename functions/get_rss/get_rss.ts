1529588d530afaa1fdd534044bc471995cc53129
nariakiiwatani <nariakiiwatani@annolab.com>
2023-06-21 15:38:42 +0900

    add CORS policy to get_rss

--- functions/get_rss/get_rss.ts
+++ functions/get_rss/get_rss.ts
@@ -1,6 +1,8 @@
 import { Handler } from '@netlify/functions'
 import fetch from 'node-fetch';
 
+const allowedOrigins = ["https://publicpodcast.link", "https://tweetpodcast.online"];
+console.info({allowedOrigins})
 export const handler: Handler = async (event, context) => {
 	const { url } = event.queryStringParameters as { url: string }
 	if (!url) {
@@ -8,18 +10,29 @@ export const handler: Handler = async (event, context) => {
 			statusCode: 400
 		}
 	}
+
+	const headers = {
+		"Access-Control-Allow-Headers": "Content-Type"
+	};
+	const origin = event.headers.origin;
+	if (origin && allowedOrigins.includes(origin)) {
+		headers['Access-Control-Allow-Origin'] = origin;
+	}
+
 	try {
 		const res = await fetch(url)
 
 		return {
 			statusCode: res.status,
 			body: await res.text(),
+			headers
 		}
 	}
 	catch(e) {
 		return {
 			statusCode: 500,
-			body: e.message
+			body: e.message,
+			headers
 		}
 	}
 }
